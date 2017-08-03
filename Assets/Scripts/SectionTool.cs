d2fc178f9d8eb2916ea2e00de5c81fcd4239c7d4
ivan.kozlovskih <ivan.kozlovskih@nival.com>
2017-08-03 20:01:39 +0300

    + Test fence ~ Almost working snapping, which is direction-inconsistent

--- Assets/Scripts/SectionTool.cs
+++ Assets/Scripts/SectionTool.cs
@@ -49,15 +49,31 @@ public class SectionTool : MonoBehaviour
         var rotation = Quaternion.LookRotation(_toPoint - _fromPoint, Vector3.up);
         var offset = Vector3.zero;
         //for (var i = 0; i < length; i++)
-        while(length > 0)
+        while (length > 0)
         {
             var instance = Instantiate(_sectionPrefabs[Random.Range(0, _sectionPrefabs.Length)]);
 
-            offset = GetPosition(instance, offset, ref length);
+            var lengthDelta = 0f;
+            var previousOffset = offset;
+
             instance.transform.position = rotation * offset;
-            offset = GetPosition(instance, offset, ref length);
+            offset = GetPosition(instance, offset, ref lengthDelta);
+            offset = GetPosition(instance, offset, ref lengthDelta);
 
             instance.transform.rotation = rotation;
+            AlignTransformToTerrain(instance.transform);
+
+            var projectedForward = instance.transform.forward;
+            projectedForward.y = 0;
+            projectedForward = projectedForward.normalized;
+
+            Debug.Log(Vector3.Dot(projectedForward, instance.transform.forward));
+
+            length += lengthDelta * Vector3.Dot(projectedForward, instance.transform.forward);
+            offset = previousOffset + (offset - previousOffset) * Vector3.Dot(projectedForward, instance.transform.forward);
+            //instance.transform.position = rotation * offset;
+            //AlignTransformToTerrain(instance.transform);
+
             instance.transform.SetParent(transform);
         }
     }
@@ -77,5 +93,71 @@ public class SectionTool : MonoBehaviour
         //offset += Vector3.forward * worldSize.z * 0.5f;
 
         return offset;
+    }
+
+    private static void AlignTransformToTerrain(Transform target)
+    {
+
+
+        var bounds = GetObjectBounds(target);
+        var localToWorldMatrix = target.localToWorldMatrix;
+
+        var samplingPoints = new List<Vector3>();
+        var extents = bounds.extents;
+        var reflectedExtents = bounds.extents;
+        reflectedExtents.x *= -1;
+
+        var center = bounds.center;
+
+        samplingPoints.Add(center + extents);
+        samplingPoints.Add(center + reflectedExtents);
+        samplingPoints.Add(center - extents);
+        samplingPoints.Add(center - reflectedExtents);
+
+        for (var i = 0; i < samplingPoints.Count; ++i)
+        {
+            var point = samplingPoints[i];
+            point.y = center.y - extents.y;
+            point = localToWorldMatrix.MultiplyPoint3x4(point);
+            point.y = Terrain.activeTerrain.SampleHeight(point);
+
+            samplingPoints[i] = point;
+
+            //Debug.DrawRay(samplingPoints[i], Vector3.up, Color.red);
+
+        }
+
+
+        var forwardA = samplingPoints[0] - samplingPoints[3];
+        var forwardB = samplingPoints[1] - samplingPoints[2];
+        var averageForward = Vector3.Lerp(forwardA, forwardB, 0.5f);
+
+        var normalA = Vector3.Cross(forwardA, samplingPoints[0] - samplingPoints[1]);
+        var normalB = Vector3.Cross(forwardB, samplingPoints[3] - samplingPoints[2]);
+        var averageNormal = Vector3.Lerp(normalA, normalB, 0.5f);
+
+        //var averageHeight = samplingPoints.Select(_ => _.y).Average();
+
+        //var targetPosition = target.position;
+        //targetPosition.y = averageHeight;
+
+        //target.position = targetPosition;
+        target.rotation = Quaternion.LookRotation(averageForward, averageNormal);
+
+        var height = Terrain.activeTerrain.SampleHeight(target.position);
+        var position = target.position;
+        position.y = height;
+        target.position = position;
+    }
+
+    private static Bounds GetObjectBounds(Transform target)
+    {
+        var meshFilter = target.GetComponentInChildren<MeshFilter>();
+        if (meshFilter != null)
+        {
+            return meshFilter.sharedMesh.bounds;
+        }
+
+        return target.GetComponentInChildren<SkinnedMeshRenderer>().sharedMesh.bounds;
     }
 }
